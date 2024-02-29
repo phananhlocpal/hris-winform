a233a3bbb2e7f6e1631364c27b3e61642c089add
phananhlocpal <anhloc280@gmail.com>
2024-02-29 19:19:02 +0700

    init project

---
+++ HRMngt/Program.cs
@@ -0,0 +1,31 @@
+﻿using HRMngt.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HRMngt
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Home view = new Home();
+
+            Application.Run(view);
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
